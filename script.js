d89c70d10be5ba4fa30168d7f40218e5574603ab
Anurag <69040347+ann29-tdk@users.noreply.github.com>
2022-12-22 20:02:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+
+// Dark or Light Images
+function imageMode(color) {
+  image1.src = `img/undraw_toy_car_${color}.svg`;
+  image2.src = `img/undraw_workout_${color}.svg`;
+  image3.src = `img/undraw_alone_${color}.svg`;
+}
+
+
+function darkMode() {
+  nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  toggleIcon.children[0].textContent = 'Dark Mode';
+  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+  imageMode('dark');
+}
+
+function lightMode() {
+  nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  toggleIcon.children[0].textContent = 'Light Mode';
+  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+
+  imageMode('light');
+}
+
+
+function switchTheme(event) {
+  if (event.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+    darkMode();
+  }
+  else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+    lightMode();
+  }
+}
+
+toggleSwitch.addEventListener('change', switchTheme);
+
+const currentTheme = localStorage.getItem('theme');
+console.log(currentTheme);
+
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
+}
+
+
+
+document.getElementById("button-1").onclick = function () {
+  location.href = "https://groove-player.netlify.app/";
+}
+document.getElementById("button-2").onclick = function () {
+  location.href = "https://ann29-tdk.github.io/hwmOrganise/";
+}
